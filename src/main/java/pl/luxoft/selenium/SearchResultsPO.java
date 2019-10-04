b5ce855adb3d54a1b1d4d339257185ea786e9701
dslt-ptc-19 <slt-ptc-19@ptc.local>
2019-10-04 16:19:55 +0200

    made some changes

--- src/main/java/pl/luxoft/selenium/SearchResultsPO.java
+++ src/main/java/pl/luxoft/selenium/SearchResultsPO.java
@@ -1,8 +1,8 @@
 package pl.luxoft.selenium;
 import org.openqa.selenium.By;
 
-public class SearchResultsPO extends BasePO {
-    final By SEARCH_TABLE_RESULTS_PO
+//public class SearchResultsPO extends BasePO {
+  //  final By SEARCH_TABLE_RESULTS_PO = By.className("alert");
 
 
-}
+//}
